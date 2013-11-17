69baf695b1dca5904bb5af75bd450461900db937
Ecayanan <ejon.cayanan@gmail.com>
2013-11-17 15:51:18 -0800

    Group Message works

--- SquirtChatClient/src/squirt/client/SquirtChatClient.java
+++ SquirtChatClient/src/squirt/client/SquirtChatClient.java
@@ -1,10 +1,13 @@
 package squirt.client;
 
+import java.util.List;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
 import javax.jms.MessageProducer;
+import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.jms.TopicPublisher;
@@ -15,39 +18,46 @@ import org.apache.activemq.ActiveMQConnection;
 
 public class SquirtChatClient implements MessageListener {
 	private MessageProducer producer;
-	//private MessageConsumer consumer;
+	private MessageConsumer consumer;
 	private Session session;
 	private TopicSubscriber subscriber;
 	private TopicPublisher publisher;
 	private ActiveMQConnection connection;
+	private List<String> userList;
+	private String user; //Using a string for user
 	
 	public SquirtChatClient(MessageProducer producer, Session session, 
 			TopicSubscriber subscriber, TopicPublisher publisher, 
-			ActiveMQConnection connection ) {
+			ActiveMQConnection connection, String user, MessageConsumer consumer ) {
 		super();
 		this.producer = producer;
-		//this.consumer = consumer;
+		this.consumer = consumer;
 		this.session = session;
 		this.subscriber = subscriber;
 		this.publisher = publisher;
 		this.connection = connection;
-		try {
+		this.user = user;
+		/*try {
 			subscriber.setMessageListener(this);
 		} catch (JMSException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		/*
+		}*/
+		
 		try {
 			consumer.setMessageListener(this);
 		} catch (JMSException e ) {
 			e.printStackTrace();
-		}*/
+		}
 	} 
 	
 	public void send(String msg) throws JMSException {
 		//producer.send(session.createTextMessage(msg));
 		publisher.send(session.createTextMessage(msg));
+	}
+	
+	public void sendUser(String msg) throws JMSException{
+		producer.send(session.createTextMessage(msg));
 	}
 	
 	public void onMessage( Message input ) {
@@ -58,5 +68,21 @@ public class SquirtChatClient implements MessageListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	public void setName(String usr){
+		this.user = usr;
+	}
+	public String getName()
+	{
+		return this.user;
+	}
+	/*
+	 * Sets the producer to send to the proper different user queue
+	 */
+	public void setProducer(String user) throws JMSException
+	{
+		Queue destQueue = session.createQueue(user);
+		MessageProducer producer = session.createProducer(destQueue);
+		this.producer = producer;
 	}
 }
--- SquirtChatClient/src/squirt/client/SquirtChatClientApplication.java
+++ SquirtChatClient/src/squirt/client/SquirtChatClientApplication.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.util.Scanner;
 
 import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
 //import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
@@ -52,7 +53,7 @@ public class SquirtChatClientApplication {
 	 * ChatClient does not depend on ActiveMQ (the concrete communication
 	 * platform we use) but just in the standard JMS interface.
 	 */
-	private static SquirtChatClient wireClient() throws JMSException,
+	private static SquirtChatClient wireClient(String user) throws JMSException,
 			URISyntaxException {
 		ActiveMQConnection connection = ActiveMQConnection.makeConnection(
 		/* Constants.USERNAME, Constants.PASSWORD, */
@@ -73,36 +74,82 @@ public class SquirtChatClientApplication {
 		TopicSubscriber subscriber = topicSession.createSubscriber(topic);
 		TopicPublisher publisher = topicSession.createPublisher(topic);
 		
-		Queue destQueue = session.createQueue(Constants.QUEUENAME);
+		Queue destQueue = session.createQueue(user);
 		MessageProducer producer = session.createProducer(destQueue);
-		return new SquirtChatClient(producer, session, subscriber, publisher, connection);
+		MessageConsumer consumer = session.createConsumer(destQueue);
+		return new SquirtChatClient(producer, session, subscriber, publisher, connection, user,consumer);
 	}
 
 	public static void main(String[] args) {
 		try {
 			
+			/*
+			 * Ask for username
+			 */
+			System.out.println("What is your username?");
+			String user;
+			Scanner scanIn = new Scanner(System.in);
+			user = scanIn.nextLine();
+	        System.out.println("ChatClient wired.");			
 			/* 
 			 * We have some other function wire the ChatClient 
 			 * to the communication platform
 			 */
-			SquirtChatClient client = wireClient();
+			
+			SquirtChatClient client = wireClient(user);
 	        System.out.println("ChatClient wired.");
-	        System.out.println("Enter a message to send: ");
-        	String message;
-        	Scanner scanIn = new Scanner(System.in);
-        	message = scanIn.nextLine();
-	        while(!(message.equals("quit"))){
-
-	        	//scanIn.close();
-	        	
-	        	/* 
-	        	 * Now we can happily send messages around
-	        	 */
-	        	client.send(message);
-	        	//client.send("Hello World!");
-	        	System.out.println("Message Sent!");
+	        boolean quit = false;
+	        while(quit == false)
+	        {
+		        System.out.println("Enter a message to send: ");
+	        	String message;
 	        	message = scanIn.nextLine();
-	        }
+	        	
+	        	if (message.equals("quit"))
+		        {
+		        	quit = true;
+		        	break;
+		        }
+	        	System.out.println("Who to send to?");
+	        	String receiver;
+	        	receiver = scanIn.nextLine();
+	        	
+		        
+	
+		        	//scanIn.close();
+		        	
+		        	/* 
+		        	 * Now we can happily send messages around
+		        	 */
+	        	int position = 0;
+	        	while(receiver.length() != 0)
+	        	{
+	        		if(receiver.indexOf(';') == -1)
+	        		{
+	        			
+	        			client.setProducer(receiver);
+			        	client.sendUser(message);
+			        	System.out.println(client.getName() + "Message Sent!");
+			        	receiver = "";
+	        		}
+	        		else
+	        		{
+	        			
+	        			String n = receiver.substring(0,receiver.indexOf(';'));
+	        			
+	        			position = receiver.indexOf(';') + 1;
+	        			
+	        			client.setProducer(n);
+			        	client.sendUser(message);
+			        	System.out.println(client.getName() + "Message Sent!");
+			        	receiver = receiver.substring(position, receiver.length());
+			        	
+			        	
+	        		}
+		        	
+		        	//message = scanIn.nextLine();
+	        	}
+	        }   
 	        scanIn.close();
 	        System.exit(0);
 		} catch (JMSException e) {
